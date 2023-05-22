a57f3b8c6e1afee47b81396e2a4ba23dbef7c040
rivaldofez <rivaldofez@gmail.com>
2023-05-23 01:31:56 +0700

    add function to save new game with spesific user

--- chromatic.swiftpm/ContentView.swift
+++ chromatic.swiftpm/ContentView.swift
@@ -7,6 +7,12 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+                .onAppear {
+//                    DatabaseManager.shared.saveNewUser(username: "rivaldo", fullname: "Rivaldo Fernandes", bio: "Ini nyoba pertama")
+                    DatabaseManager.shared.addNewGame(username: "rivaldo", level: 10)
+                    
+                    print(DatabaseManager.shared.getUserData())
+                }
         }
     }
 }
--- chromatic.swiftpm/Manager/DatabaseManager.swift
+++ chromatic.swiftpm/Manager/DatabaseManager.swift
@@ -7,3 +7,56 @@
 
 import Foundation
 import RealmSwift
+
+class DatabaseManager {
+    static let shared = DatabaseManager()
+    
+    func saveNewUser(username: String, fullname: String, bio: String = ""){
+        let realm = try! Realm()
+        
+        let user = UserEntity()
+        user.username = username
+        user.fullname = fullname
+        user.bio = bio
+        user.games = List<GameEntity>()
+        
+        do {
+            try realm.write {
+                realm.add(user)
+            }
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func getUserData() -> [UserEntity] {
+        let realm = try! Realm()
+        
+        let dataUser = realm.objects(UserEntity.self)
+            .sorted(byKeyPath: "username", ascending: false)
+        
+        return dataUser.map { $0 }
+    }
+    
+    
+    func addNewGame(username: String, level: Int){
+        let realm = try! Realm()
+        
+        let resultUser = realm.objects(UserEntity.self)
+            .where {  $0.username == username}
+        
+        if let user = resultUser.first {
+            let game = GameEntity()
+            game.id = UUID().uuidString
+            game.level = level
+            
+            do {
+                try realm.write {
+                    user.games.append(game)
+                }
+            } catch let error as NSError {
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
