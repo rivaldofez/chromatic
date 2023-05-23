34140f42afa7e25535157f68b4d49eff7cfde4e8
rivaldofez <rivaldofez@gmail.com>
2023-05-23 12:48:19 +0700

    add color and create extension

---
+++ chromatic.swiftpm/Extension/Color+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Rivaldo Fernandes on 23/05/23.
+//
+
+import SwiftUI
+
+extension Color {
+    static let primaryAccent = Color("PrimaryAccent")
+    static let primaryAccentLabel = Color("PrimaryAccentLabel")
+    static let primaryLabel = Color("PrimaryLabel")
+    static let primaryButtonLabel = Color("PrimaryButtonLabel")
+    static let gray = Color("Gray")
+}
