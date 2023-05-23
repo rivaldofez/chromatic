b3a7f6ed27ec7276d574acc83b88229936d731e3
rivaldofez <rivaldofez@gmail.com>
2023-05-23 12:50:53 +0700

    add custom view back button

---
+++ chromatic.swiftpm/View/CustomView/BackButtonView.swift
@@ -0,0 +1,32 @@
+//
+//  File.swift
+//  
+//
+//  Created by Rivaldo Fernandes on 23/05/23.
+//
+
+import SwiftUI
+
+struct BackButtonView: View {
+    var action: () -> Void
+    
+    var body: some View {
+        Button(action: action){
+            Image(systemName: "chevron.left")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 25, height: 25)
+                .padding()
+                .foregroundColor(.primaryAccent)
+                .background(.gray.opacity(0.2))
+                .clipShape(Circle())
+        }
+        .hLeading()
+    }
+}
+
+struct BackButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        BackButtonView(action: {})
+    }
+}
